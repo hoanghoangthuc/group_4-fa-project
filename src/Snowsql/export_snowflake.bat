c6ce14c7e0a3fea629e19b586a429a20f675828a
Hoàng Hoàng Thúc <hoangthuc10a4@gmail.com>
2021-08-18 18:10:43 +0700

    unload data from snowflake
    fix unload bat file and snowsql code
    
    Co-Authored-By: matornat2812 <62283838+matornat2812@users.noreply.github.com>

--- src/Snowsql/export_snowflake.bat
+++ src/Snowsql/export_snowflake.bat
@@ -9,7 +9,7 @@ cls
 
 echo Export Start>> export_log.txt
 
-snowsql -a yr27995.southeast-asia.azure -u ThucHH -d PROJECT1 -w STAGE -s MODEL -r SYSADMIN -f %file_path%\export_snowsql.sql -D folder=%folder% -o output_file=export_log.txt
+snowsql -a yr27995.southeast-asia.azure -u ThucHH -d PROJECT1 -w WAREHOUSE -s MODEL -r SYSADMIN -f %file_path%\export_snowsql.sql -D folder=%folder% -o output_file=export_log.txt
 
 echo Export Complete>> export_log.txt
 echo --------------------------------------------------------------------------------------->> export_log.txt
--- src/Snowsql/export_snowsql.sql
+++ src/Snowsql/export_snowsql.sql
@@ -2,30 +2,34 @@
 
 
 select CURRENT_TIMESTAMP, CURRENT_USER;
-select 'Export file to /Export_snowflake' as Process;
 
+select 'export DIMDATE from snowflake' as Process;
+COPY INTO @%DIMDATE/Export_snowflake/DIMDATE.csv from PROJECT1.MODEL.DIMDATE file_format = (format_name ='CSV_FILE' compression=none) single = True max_file_size = 4900000000 OVERWRITE=TRUE;
+GET @%DIMDATE/Export_snowflake/DIMDATE.csv file://&{folder};
 
---PUT file:///&{folder}/*Warehouse*.csv @%Warehouse;
-COPY INTO '@%DIMDATE/Export_snowflake/' from PROJECT1.MODEL.DIMDATE file_format = (format_name ='CSV_FILE' compression='GZIP') OVERWRITE=TRUE;
-GET @%DIMDATE/Export_snowflake/ file://&{folder};
+select 'export DIMCUSTOMER from snowflake' as Process;
+COPY INTO @%DIMCUSTOMER/Export_snowflake/DIMCUSTOMER.csv from PROJECT1.MODEL.DIMCUSTOMER file_format = (format_name ='CSV_FILE' compression=none) single = True max_file_size = 4900000000 OVERWRITE=TRUE;
+GET @%DIMCUSTOMER/Export_snowflake/DIMCUSTOMER.csv file://&{folder};
 
-COPY INTO '@%DIMCUSTOMER/Export_snowflake/' from PROJECT1.MODEL.DIMCUSTOMER file_format = (format_name ='CSV_FILE' compression='GZIP') OVERWRITE=TRUE;
-GET @%DIMCUSTOMER/Export_snowflake/ file://&{folder};
+select 'export DIMLOCATION_WAREHOUSE from snowflake' as Process;
+COPY INTO @%DIMLOCATION_WAREHOUSE/Export_snowflake/DIMLOCATION_WAREHOUSE.csv from PROJECT1.MODEL.DIMLOCATION_WAREHOUSE file_format = (format_name ='CSV_FILE' compression=none) single = True max_file_size = 4900000000 OVERWRITE=TRUE;
+GET @%DIMLOCATION_WAREHOUSE/Export_snowflake/DIMLOCATION_WAREHOUSE.csv file://&{folder};
 
-COPY INTO '@%DIMLOCATION_WAREHOUSE/Export_snowflake/' from PROJECT1.MODEL.DIMLOCATION_WAREHOUSE file_format = (format_name ='CSV_FILE' compression='GZIP') OVERWRITE=TRUE;
-GET @%DIMLOCATION_WAREHOUSE/Export_snowflake/ file://&{folder};
+select 'export DIMLOCATION_CUSTOMER from snowflake' as Process;
+COPY INTO @%DIMLOCATION_CUSTOMER/Export_snowflake/DIMLOCATION_CUSTOMER.csv from PROJECT1.MODEL.DIMLOCATION_CUSTOMER file_format = (format_name ='CSV_FILE' compression=none) single = True max_file_size = 4900000000 OVERWRITE=TRUE;
+GET @%DIMLOCATION_CUSTOMER/Export_snowflake/DIMLOCATION_CUSTOMER.csv file://&{folder};
 
-COPY INTO '@%DIMLOCATION_CUSTOMER/Export_snowflake/' from PROJECT1.MODEL.DIMLOCATION_CUSTOMER file_format = (format_name ='CSV_FILE' compression='GZIP') OVERWRITE=TRUE;
-GET @%DIMLOCATION_CUSTOMER/Export_snowflake/ file://&{folder};
+select 'export DIMPRODUCT from snowflake' as Process;
+COPY INTO @%DIMPRODUCT/Export_snowflake/DIMPRODUCT.csv from PROJECT1.MODEL.DIMPRODUCT file_format = (format_name ='CSV_FILE' compression=none) single = True max_file_size = 4900000000 OVERWRITE=TRUE;
+GET @%DIMPRODUCT/Export_snowflake/DIMPRODUCT.csv file://&{folder};
 
-COPY INTO '@%DIMPRODUCT/Export_snowflake/' from PROJECT1.MODEL.DIMPRODUCT file_format = (format_name ='CSV_FILE' compression='GZIP') OVERWRITE=TRUE;
-GET @%DIMPRODUCT/Export_snowflake/ file://&{folder};
+select 'export DIMWAREHOUSE from snowflake' as Process;
+COPY INTO @%DIMWAREHOUSE/Export_snowflake/DIMWAREHOUSE.csv from PROJECT1.MODEL.DIMWAREHOUSE file_format = (format_name ='CSV_FILE' compression=none) single = True max_file_size = 4900000000 OVERWRITE=TRUE;
+GET @%DIMWAREHOUSE/Export_snowflake/DIMWAREHOUSE.csv file://&{folder};
 
-COPY INTO '@%DIMWAREHOUSE/Export_snowflake/' from PROJECT1.MODEL.DIMWAREHOUSE file_format = (format_name ='CSV_FILE' compression='GZIP') OVERWRITE=TRUE;
-GET @%DIMWAREHOUSE/Export_snowflake/ file://&{folder};
-
-COPY INTO '@%FACTRECORD/Export_snowflake/' from PROJECT1.MODEL.FACTRECORD file_format = (format_name ='CSV_FILE' compression='GZIP') OVERWRITE=TRUE;
-GET @%FACTRECORD/Export_snowflake/ file://&{folder};
+select 'export FACTRECORD from snowflake' as Process;
+COPY INTO @%FACTRECORD/Export_snowflake/FACTRECORD.csv from PROJECT1.MODEL.FACTRECORD file_format = (format_name ='CSV_FILE' compression=none) single = True max_file_size = 4900000000 OVERWRITE=TRUE;
+GET @%FACTRECORD/Export_snowflake/FACTRECORD.csv file://&{folder};
 
 
 
